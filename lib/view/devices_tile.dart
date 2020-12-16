bad38266a28db1e6639bcec98115607e4154c8ec
Marcelo Amorim <mamorim813@gmail.com>
2020-12-15 21:47:46 -0300

    Add devices_tile.dart

--- lib/view/devices_tile.dart
+++ lib/view/devices_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:realtimedatabase_teste/model/device_data.dart';
+import 'package:realtimedatabase_teste/view/measures_tile.dart';
 
 class DevicesTile extends StatelessWidget {
 
@@ -9,6 +10,72 @@ class DevicesTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      child: Container(
+        height: 150.0,
+        child: Column(
+          children: [
+            Container(
+              height: 50,
+              child: Stack(
+                children: [
+                  Positioned(
+                      child: Text(
+                        deviceData.name,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25.0,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      left: 150.0,
+                      top: 8.0
+                  ),
+                  Positioned(
+                    child: IconButton(
+                      icon: Icon(Icons.refresh),
+                      onPressed: (){},
+                    ),
+                    right: 70.0,
+                  ),
+                  Positioned(
+                    child: IconButton(
+                      icon: Icon(Icons.list_alt),
+                      onPressed: (){},
+                    ),
+                    right: 35.0,
+                  ),
+                  Positioned(
+                    child: IconButton(
+                      icon: Icon(Icons.settings),
+                      onPressed: (){},
+                    ),
+                    right: 0.0,
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                    child: SizedBox(
+                      height: 50.0,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: deviceData.measures[0].measures.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index){
+                          return MeasuresTile(deviceData.measures[0], index);
+                        },
+                      ),
+                    )
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:realtimedatabase_teste/controller/device_controller.dart';
-
-import 'measures_tile.dart';
+import 'package:realtimedatabase_teste/view/devices_tile.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -39,73 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     shrinkWrap: true,
                     itemCount: DeviceController.devices.length,
                     itemBuilder: (context, index) {
-                      return Card(
-                        child: Container(
-                          height: 150.0,
-                          child: Column(
-                            children: [
-                              Container(
-                                height: 50,
-                                child: Stack(
-                                  children: [
-                                    Positioned(
-                                      child: Text(
-                                        DeviceController.devices[index].name,
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 25.0,
-                                        ),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                      left: 150.0,
-                                      top: 8.0
-                                    ),
-                                    Positioned(
-                                      child: IconButton(
-                                        icon: Icon(Icons.refresh),
-                                        onPressed: (){},
-                                      ),
-                                      right: 70.0,
-                                    ),
-                                    Positioned(
-                                      child: IconButton(
-                                        icon: Icon(Icons.list_alt),
-                                        onPressed: (){},
-                                      ),
-                                      right: 35.0,
-                                    ),
-                                    Positioned(
-                                      child: IconButton(
-                                        icon: Icon(Icons.settings),
-                                        onPressed: (){},
-                                      ),
-                                      right: 0.0,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Flexible(
-                                    child: SizedBox(
-                                      height: 50.0,
-                                      child: ListView.builder(
-                                        shrinkWrap: true,
-                                        itemCount: DeviceController.devices[index].measures[0].measures.length,
-                                        scrollDirection: Axis.horizontal,
-                                        itemBuilder: (context, index2){
-                                          return MeasuresTile(DeviceController.devices[index].measures[0], index2);
-                                        },
-                                      ),
-                                    )
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
+                      return DevicesTile(DeviceController.devices[index]);
                     },
                   ),
                 ),

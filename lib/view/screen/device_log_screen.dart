9906bd69b306c89dbf4c2695042d7dff77518c72
Marcelo Amorim <mamorim813@gmail.com>
2020-12-17 18:05:03 -0300

    Update device_log_screen.dart | Update Widgets and variables

--- lib/view/screen/device_log_screen.dart
+++ lib/view/screen/device_log_screen.dart
@@ -18,107 +18,45 @@ class _DeviceLogScreenState extends State<DeviceLogScreen> {
   //device data to get device info
   final DeviceData deviceData;
 
-  //constructor to receive deiceData
-  _DeviceLogScreenState(this.deviceData);
+  //variables
+  String deviceName;
 
-  //global key to access the form at the sign up button
-  final _formKey = GlobalKey<FormState>();
+  //constructor to receive deiceData
+  _DeviceLogScreenState(this.deviceData){
+    deviceName = deviceData.name;
+  }
 
   //global key to access the scaffold at onSuccess and onFail functions
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
-  //text field controllers
-  final _nameController = TextEditingController();
-
   //device controller
   final DeviceController deviceController = DeviceController();
-
-  //bool to see if the device has been edited
-  bool _deviceEdited = false;
-
-  //Function to get deviceData info
-  void _getDeviceDataInfo(){
-    _nameController.text = deviceData.name;
-  }
-
-  //Function to load page
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance
-        .addPostFrameCallback((_) => _getDeviceDataInfo());
-  }
 
   @override
   Widget build(BuildContext context) {
 
     return WillPopScope(
-      onWillPop: (){},
+      onWillPop: (){
+        Navigator.pop(context);
+        return Future.value(false);
+      },
       child: Scaffold(
         key: _scaffoldKey,
         appBar: AppBar(
           title: Text("Device Log"),
           centerTitle: true,
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            if(_formKey.currentState.validate() && _deviceEdited) {
-              //device map
-              Map<String, dynamic> deviceData;
-
-              deviceData = {
-                'name': _nameController.text,
-              };
-
-            }
-          },
-          child: Icon(Icons.save),
-          backgroundColor: Theme.of(context).primaryColor,
-        ),
-        body: Form(
-          key: _formKey,
-          child: ListView(
-            padding: EdgeInsets.all(20.0),
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Name',
-                    style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).primaryColor
-                    ),
-                  ),
-                  SizedBox(height: 3.0),
-                  TextFormField( //full name text field
-                    controller: _nameController,
-                    decoration: InputDecoration(
-                      hintText: 'Put device name',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: BorderSide(
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: BorderSide(
-                            color: Theme.of(context).primaryColor,
-                            width: 1.3
-                        ),
-                      ),
-                    ),
-                    validator: (text){ //rule to validate the input data
-                      if(text.isEmpty) return 'Invalid Name!';
-                      else return null;
-                    },
-                    maxLength: 20,
-                    onChanged: (text){
-                      _deviceEdited = true;
-                    },
-                  ),
-                ],
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Text(
+                deviceName,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                ),
               )
             ],
           ),
